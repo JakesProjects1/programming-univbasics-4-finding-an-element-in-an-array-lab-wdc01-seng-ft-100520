d0d283f800b615e93e7f0d8e0b73f64150589a1b
Jacob Kagon <jacob@kagon.net>
2020-09-14 21:53:03 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,7 +1,8 @@
  scale = [2,3,4,5]
 
 def find_element_index(array, value_to_find)
-  array(value_to_find)
+  while value_to_find < array.length do
+    if value_to_find = 
 end
 
 find_element_index(scale, 2)
